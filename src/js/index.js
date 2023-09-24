43526bbaf5735235415938af0588b3e565727d92
tur104 <73474339+tur104@users.noreply.github.com>
2023-09-24 16:08:20 +0300

    header eng

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,17 @@
-import mobileNav from './modules/mobile-nav.js';
-mobileNav();
+// import autoCompleteFunc from "./modules/autoComplete";
+
+// autoCompleteFunc();
+
+import { easepick, TimePlugin} from '@easepick/bundle';
+const picker = new easepick.create({
+    element: "#datePicker",
+    css: [
+        "https://cdn.jsdelivr.net/npm/@easepick/bundle@1.2.1/dist/index.css"
+    ],
+    format: "HH:mm, DD/MM/YY",
+    plugins: [TimePlugin],
+})
+
+
+// import mobileNav from './modules/mobile-nav.js';
+// mobileNav();
---
+++ src/js/modules/autoComplete.js
@@ -0,0 +1,32 @@
+import countryList from "./../helpers/countyList";
+import autoComplete from "@tarekraafat/autocomplete.js";
+
+
+function autoCompleteFunc() {
+
+    const autoCompleteJS = new autoComplete({
+        selector: '#autoCompleteContry',
+        placeHolder: "e.g Bali, Indonesia",
+        data: {
+            src: countryList,
+            caches: true,
+        },
+        resultItem: {
+            highlight: true,
+        },
+        events: {
+            input: {
+                selection: (event) => {
+                    const selection = event.detail.selection.value;
+                    autoCompleteJS.input.value = selection;
+                }
+            }
+        },
+
+        searEngine: 'strict',
+    });
+
+}
+
+
+export default autoCompleteFunc;
