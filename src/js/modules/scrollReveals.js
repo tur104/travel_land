634a7f329bd65e2619ab0395fd15c4e71eecf65b
tur104 <73474339+tur104@users.noreply.github.com>
2023-10-18 23:21:29 +0300

    fix scrollReveals

--- src/js/modules/scrollReveals.js
+++ src/js/modules/scrollReveals.js
@@ -1,26 +1,32 @@
 import ScrollReveal from 'scrollreveal';
 
+ScrollReveal({
+    distance: '80px',
+    duration: 2000,
+});
 
 function scrollRevealFunc() {
-    ScrollReveal({
-        distance: '80px',
-        duration: 2000,
+    ScrollReveal().reveal(`.header, .partners`, {
+        distance: '0px',
+        opacity: 0,
     });
 
-
-    ScrollReveal().reveal(`.header, .partners`, {
+    ScrollReveal().reveal(`.discover__picture-hint`, {
         origin: 'top',
     });
 
-    ScrollReveal().reveal(`.discover__picture-hint, .discover__title`, {
+    ScrollReveal().reveal(`.discover__desc`, {
         origin: 'left',
     });
 
-    ScrollReveal().reveal(`.discover__picture-scroll, .discover__text `, {
-        origin: 'right',
-    });
+    ScrollReveal().reveal(
+        `.discover__picture-img`,
+        {
+            origin: 'right',
+        }
+    );
 
-    ScrollReveal().reveal(` .discover__picture-img `, {
+    ScrollReveal().reveal(`.discover__picture-scroll`, {
         origin: 'bottom',
     });
 }
