e0f4d5838a67c413133c223c994ed02bce51e0d5
tur104 <73474339+tur104@users.noreply.github.com>
2023-10-18 23:18:44 +0300

    fix swiper testimonials

--- src/js/modules/swepir-testimonials.js
+++ src/js/modules/swepir-testimonials.js
@@ -1,50 +1,68 @@
 import Swiper from 'swiper/bundle';
 import 'swiper/css/bundle';
 
-function swiperFunc() {
-    const swiper = new Swiper('#testimonials-col-1', {
-        direction: 'vertical',
-        slidesPerView: 'auto',
-        spaceBetween: 32,
-        grabCursor: true,
-        a11y: false,
-        freeMode: true,
-        speed: 2000,
-        loop: true,
-        autoplay:{
-            delay: 0.5,
-            disableOnInteraction: false,
-        },        
-    });
-    
-    const swiper2 = new Swiper('#testimonials-col-2', {
-        direction: 'vertical',
-        slidesPerView: 'auto',
-        spaceBetween: 32,
-        grabCursor: true,
-        a11y: false,
-        freeMode: true,
-        speed: 2000,
-        loop: true,
-        autoplay:{
-            delay: 0.5,
-            disableOnInteraction: false,
-        },        
-    });
+let swiper1, swiper2, swiper3, swiperHorizontal;
 
-    const swiper3 = new Swiper('#testimonials-col-3', {
-        direction: 'vertical',
-        slidesPerView: 'auto',
-        spaceBetween: 32,
-        grabCursor: true,
-        a11y: false,
-        freeMode: true,
-        speed: 2000,
-        loop: true,
-        autoplay:{
-            delay: 0.5,
-            disableOnInteraction: false,
-        },        
-    });
+const configVertical = {
+    direction: 'vertical',
+    slidesPerView: 'auto',
+    spaceBetween: 0,
+    grabCursor: true,
+    a11y: false,
+    freeMode: true,
+    speed: 2000,
+    loop: true,
+    autoplay: {
+        delay: 0.0,
+        disableOnInteraction: false,
+    },
 }
+
+const configHorizontal = {
+    direction: 'horizontal',
+    // slidesPerView: 1,
+    slidesPerView: 'auto',
+    spaceBetween: 10,
+    grabCursor: true,
+    a11y: false,
+    freeMode: true,
+    speed: 2000,
+    loop: true,
+    autoplay: {
+        delay: 0.0,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+		768: {
+			slidesPerView: 2,
+			spaceBetween: 32,
+		},
+        374: {
+            spaceBetween: 20,
+		},
+	},
+    
+}
+
+const breakpoint = window.matchMedia('(width < 1024px)');
+
+const breakpointChecker = function () {
+    if (breakpoint.matches === true) {
+        swiperHorizontal = new Swiper('#testimonials-horizontal-swiper', configHorizontal)
+        if (swiper1 !== undefined) swiper1.destroy(true, true);
+        if (swiper2 !== undefined) swiper2.destroy(true, true);
+        if (swiper3 !== undefined) swiper3.destroy(true, true);
+        return;
+    } else if (breakpoint.matches === false) {
+        swiper1 = new Swiper('#testimonials-col-1', configVertical);
+        swiper2 = new Swiper('#testimonials-col-2', configVertical);
+        swiper3 = new Swiper('#testimonials-col-3', configVertical);
+        if (swiperHorizontal !== undefined) swiperHorizontal.destroy(true, true);
+
+        return
+    }
+}
+breakpoint.addListener(breakpointChecker);
+breakpointChecker();
+
 export default swiperFunc;
